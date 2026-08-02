26b528638428e8ac14221848420c268107aba3e4
nijum-dev <jannatnijum.dev@gmail.com>
2026-08-02 21:18:25 +0600

    deployment done

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '10mb' })); // Allow larger payloads for base64 images
+app.use(express.json({ limit: '10mb' })); 
 
 // MongoDB client
 const client = new MongoClient(uri, {
